482c946005472ee4dafa4d51a17848ba4a4f0ff6
Harmeet <hs978698@gmail.com>
2024-05-05 13:56:44 +0530

    selection sort

---
+++ search.c
@@ -0,0 +1,19 @@
+// search a num in arr
+#include<stdio.h>
+int main(){
+     int n,x;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+     
+    scanf("%d",&x);
+    int i;
+    for(i=0;i<n;i++){
+    if (arr[i]==x){
+        printf("yes its present");
+    }}
+
+    return 0;
+}
