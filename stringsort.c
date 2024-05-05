0e88e0c54115f6251baf3438cc14cd8bf5f14f06
Harmeet <hs978698@gmail.com>
2024-05-05 14:28:02 +0530

    modified stringsort.c

--- stringsort.c
+++ stringsort.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-void sortString(char* str) {
-    int length = strlen(str);
-    char temp;
-
-    for (int i = 0; i < length - 1; i++) {
-        for (int j = i + 1; j < length; j++) {
-            if (str[i] > str[j]) {
-                temp = str[i];
-                str[i] = str[j];
-                str[j] = temp;
-            }
-        }
-    }
-}
-
 int main() {
     char str[100];
+    int length, i, j;
+    char temp;
 
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
@@ -25,7 +12,17 @@ int main() {
     // Remove the newline character from the input
     str[strcspn(str, "\n")] = '\0';
 
-    sortString(str);
+    length = strlen(str);
+
+    for (i = 0; i < length - 1; i++) {
+        for (j = i + 1; j < length; j++) {
+            if (str[i] > str[j]) {
+                temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
 
     printf("Sorted string: %s\n", str);
 
