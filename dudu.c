35e71ef6a021e12403968fcb32729ea205802066
Harmeet <hs978698@gmail.com>
2024-05-05 21:45:41 +0530

    dudu was created just for experiment

---
+++ dudu.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    int result = factorial(num);
+    printf("The factorial of %d is %d\n", num, result);
+    
+    return 0;
+}
