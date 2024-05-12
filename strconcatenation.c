982bc7964b16f9fd9aa7c47561ff4f0f3f20d16d
Harmeet <hs978698@gmail.com>
2024-05-12 12:37:09 +0530

    strconcat modified

--- strconcatenation.c
+++ strconcatenation.c
@@ -12,20 +12,21 @@ int main(){
 
     // strcat(str1, str2);
     // printf("%s%s", str1, str2); // Print both strings on the same line
-    char string1[100];
-char string2[100];
-printf("Enter the first string: ");
-scanf("%s", string1);
-printf("Enter the second string: ");
-scanf("%s", string2);
+        char string1[100];
+        char string2[100];
+        printf("Enter the first string: ");
+        scanf("%s", string1);
+        printf("Enter the second string: ");
+        scanf("%s", string2);
+        printf("%s%s\n", string1, string2);
 
-// Concatenate the strings
-char concatenated_string[200];
-strcpy(concatenated_string, string1);
-strcat(concatenated_string, "");
-strcat(concatenated_string, string2);
 
-// Print the concatenated string
-printf("%s\n", concatenated_string);
-    return 0;
-}
+    //     // Concatenate the strings
+    //     char concatenated_string[200];
+    //     strcpy(concatenated_string, string1);
+    //     strcat(concatenated_string, string2);
+
+    //     // Print the concatenated string
+    //     printf("%s\n", concatenated_string);
+        return 0;
+    }
