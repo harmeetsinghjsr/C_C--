2b4ae077145d167824e66402428cd7ce34ec0a5b
Harmeet <hs978698@gmail.com>
2024-05-12 12:38:42 +0530

    idk

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,23 +1,45 @@
+// #include<stdio.h>
+// #include<math.h>
+// #include<stdlib.h>
+// int main(){
+//     int num,temp1,temp2,d,c=0,sum=0;
+//     scanf("%d",&num);
+//     temp1=num;
+//     if(num == 0){
+//         c = 1;
+//     }
+//     else{
+//         while(temp1!=0){
+//             d=temp1%10;
+//             if(d==0){
+//                 c++;
+//             }
+//             else
+//             break;
+//         }
+//     }
+//     printf("Trailing zeros: %d\n",c);
+// }
+#include <stdio.h>
 
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n][n];
-    int i,j;
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            scanf("%d",&arr[i][j]);
-        }
+int main() {
+    int num, count = 0;
 
-    }
-    for(i=0;i<n;i++){
-        int sum=0;
-        for(j=0;j<(i=j);j++){
-            sum =sum + arr[i][j];
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &num);
+
+    // Handle the special case of 0
+    if (num == 0) {
+        count = 1;
+    } else {
+        // Efficiently count trailing zeros using a loop and division
+        while (num % 10 == 0) {
+            count++;
+            num /= 10;  // Remove the factor of 5 using division
         }
-        printf("sum of diagonal element is %d",sum);
     }
+
+    printf("The number of trailing zeros in %d is %d\n", num, count);
 
     return 0;
 }
